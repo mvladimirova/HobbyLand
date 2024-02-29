a8d4b52529df030f7b60f9450ae018c5babc0adc
Marianna Karaargirova <marianna.vl.vladimirova@gmail.com>
2024-02-29 14:30:22 +0200

    Add node BE app

---
+++ apps/hobby-land-api/jest.config.ts
@@ -0,0 +1,11 @@
+/* eslint-disable */
+export default {
+  displayName: 'hobby-land-api',
+  preset: '../../jest.preset.js',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/hobby-land-api',
+};
