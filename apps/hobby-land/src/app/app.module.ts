3f76880ebd70fc2114aadcd3fd5adee668a90b10
Marianna Karaargirova <marianna.vl.vladimirova@gmail.com>
2024-03-01 21:22:13 +0200

    remove standalone component

---
+++ apps/hobby-land/src/app/app.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { AppComponent } from './app.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [BrowserModule, HttpClientModule],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
