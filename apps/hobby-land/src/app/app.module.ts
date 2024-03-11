ab76dbb08c6da14e49b6feadcda2c54131ec145e
Marianna Karaargirova <marianna.vl.vladimirova@gmail.com>
2024-03-11 17:55:14 +0200

    add header and sidenav

--- apps/hobby-land/src/app/app.module.ts
+++ apps/hobby-land/src/app/app.module.ts
@@ -1,11 +1,21 @@
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
+
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, HttpClientModule],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    MatToolbarModule,
+    MatSidenavModule,
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
