c6eac232098e465b4ba6161ecb42ae54804eadb8
Marianna Karaargirova <marianna.vl.vladimirova@gmail.com>
2024-03-18 18:23:19 +0200

    scoreboard header

--- apps/hobby-land/src/app/modules/score-board/score-board.module.ts
+++ apps/hobby-land/src/app/modules/score-board/score-board.module.ts
@@ -2,13 +2,21 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { MatTableModule } from '@angular/material/table';
+import { MatButtonModule } from '@angular/material/button';
 
 import { ScoreBoardRoutingModule } from './score-board-routing.module';
 import { ScoreBoardComponent } from './components/score-board/score-board.component';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [ScoreBoardComponent],
-  imports: [CommonModule, ScoreBoardRoutingModule, MatTableModule],
+  imports: [
+    CommonModule,
+    ScoreBoardRoutingModule,
+    MatTableModule,
+    MatButtonModule,
+    MatIconModule,
+  ],
   exports: [ScoreBoardComponent],
 })
 export class ScoreBoardModule {}
